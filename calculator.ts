36552e46a242a4d07ff041e434e5f6c205b885e2
Matias Arno <amatiasarno@gmail.com>
2023-07-15 10:14:20 -0300

    first commit

---
+++ calculator.ts
@@ -0,0 +1,7 @@
+export const calculadora = {
+	sumar: (a: number, b: number) => a + b,
+	restar: (a: number, b: number) => a - b,
+	multiplicar: (a: number, b: number) => a * b,
+	dividir: (a: number, b: number) => a / b,
+	potenciar: (a: number, b: number) => a ** b,
+};
